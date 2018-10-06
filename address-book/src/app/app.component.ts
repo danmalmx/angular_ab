b7506c63e8046a04bb6660af6b4e017cac64bad6
danmalmx <danmalmx@gmail.com>
2018-10-06 21:17:49 +0200

    adding method for cubmit button

--- address-book/src/app/app.component.ts
+++ address-book/src/app/app.component.ts
@@ -6,19 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  contact: any;
-
-  constructor(){
-    this.contact = {
-      name: ``,
-      email: ``,
-      company: ``,
-      role: ``,
-      twitter: ``,
-      location: ``,
-      notes: ``
-    }
-  }
   title = 'address-book';
 
   createNewContact() {
